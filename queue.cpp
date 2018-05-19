a9d29ad23ff2b73ee823b8cb14b5765c9198e4a3
spade <spadeaiverxin@163.com>
2018-05-19 23:42:01 +0800

    Queue finished！

--- queue.cpp
+++ queue.cpp
@@ -1,7 +1,7 @@
 #include "queue.h"
 Status InitQueue(LinkQueue &Q)
 {
-    // 构造一个空队列
+    // 构造一个空队列 ,有空数据的头结点
     Q.front = Q.rear = (QueuePtr) malloc(sizeof (QNode));
     if (!Q.front)  exit(OVERFLOW);
     Q.front->next=NULL;
@@ -29,5 +29,53 @@ Status EnQueue(LinkQueue &Q,QElemType e)
     Q.rear->next=p;
     Q.rear=p;
     return OK;
+}
+
+Status Dequeue(LinkQueue &Q , QElemType &e)
+{
+    //删除Q的队头元素，用e返回删除的值
+    if(Q.front==Q.rear) return ERROR;
+    QueuePtr p;
+    p=Q.front->next; //删除的节点
+    e=Q.front->data; //删除节点的数据
+    Q.front->next=p->next; //头结点重新链接
+    if(Q.rear == p) Q.rear=Q.front; //如果删除的是最后一个队尾元素\
+                                    重新定义队尾指针的指向
+    free(p);
+    return OK;
+}
+
+Status InitSqQueue(SqQueue & Q)
+{
+    // 构造一个空队列Q
+    Q.base = (QElemType *) malloc(MAXQSIZE*sizeof(QElemType));
+    if (!Q.base) exit(OVERFLOW);
+    Q.front = Q.rear = 0 ;
+    return OK ; 
+}
+
+int SqQueueLength(SqQueue Q)
+{
+    // 返回Q的元素个数，即队列的长度
+    return (Q.rear - Q.front + MAXQSIZE) % MAXQSIZE ; 
+}
+
+int SqEnQueue(SqQueue &Q , QElemType e)
+{
+    // 插入元素e在新的对尾元素
+    if ((Q.rear+1)%MAXQSIZE == Q.front) return ERROR ; //队列满。有一个空的应该是
+    Q.base[Q.rear] = e;
+    Q.rear=(Q.rear+1) % MAXQSIZE;
+    return OK;
+}
+
+Status DeQueue(SqQueue &Q , QElemType & e)
+{
+    //若队列不空，则删除Q的队头元素，用e返回其值，并且返回OK
+    //否则返回error
+    if (Q.front == Q.rear) return ERROR;
+    e = Q.base[Q.front];
+    Q.front=(Q.front+1) % MAXQSIZE;
+    return OK;    
 }
 
--- queue.h
+++ queue.h
@@ -17,6 +17,21 @@ typedef struct {
     QueuePtr rear;
 }LinkQueue;
 
+
+/* 循环队列---队列的顺序存储结构 */
+#define MAXQSIZE 100    //最大队列长度
+
+typedef struct 
+{
+    QElemType * base ; // 初始化的动态分配存储空间
+    int front ; //头指针， 若队列不空，只想队列头元素
+    int rear ;  //尾指针，队列不空时，指向队列尾元素的下一个位置
+}SqQueue;
+
+
+
+
+
 Status InitQueue(LinkQueue &Q);
 Status DestoryQueue(LinkQueue &Q);
 Status ClearQueue(LinkQueue &Q);
